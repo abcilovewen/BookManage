17e6f47fbbd2841d491a2409d9f34cc07d43dd0a
maozh <12875738632@qq.com>
2020-01-04 15:42:52 +0800

    管理员信息管理

--- src/main/java/com/briup/bookmanage/service/IBorrowService.java
+++
@@ -1,9 +0,0 @@
-package com.briup.bookmanage.service;
-
-import com.briup.bookmanage.bean.Borrow;
-
-import java.util.List;
-
-public interface IBorrowService {
-    List<Borrow> findAll() throws RuntimeException;
-}
